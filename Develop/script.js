ccc3d646d6874cfa0b0772576754504aab5f8ce8
DariaPressley <daria.mik@gmail.com>
2023-09-19 19:09:54 -0700

    updating javascript so that tasks are stored into local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-var formEl = $(".description");
-
-
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('MMM D, YYYY'));
 
@@ -34,21 +31,20 @@ $(document).ready(function () {
   updateHourlyBlocks();
 });
 
+function saveText(hour) {
+  var taskArea = $(`#hour-${hour} .description`);
+  localStorage.setItem(`hour-${hour}-text`, taskArea.val());
+}
 
+function loadText(hour) {
+  var taskArea = $(`#hour-${hour} .description`);
+  var savedText = localStorage.getItem(`hour-${hour}-text`);
+  if (savedText !== null) {
+    taskArea.val(savedText);
+  }
+}
 
-
-
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
+$(".saveBtn").on("click", function () {
+  var hour = parseInt($(this).closest(".row").attr("id").replace("hour-", ""));
+  saveText(hour);
 });
