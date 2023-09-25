3454f961573b15142f2863e4e8bec4112370f4d2
DariaPressley <daria.mik@gmail.com>
2023-09-25 15:26:31 -0700

    updating code

--- Develop/script.js
+++ Develop/script.js
@@ -3,22 +3,39 @@ $('#currentDay').text(currentDay.format('MMM D, YYYY'));
 
 
 $(document).ready(function () {
-  function updateHourlyBlocks() {
-    var currentHour = dayjs().hour();
-    for (var i = 9; i <= 17; i++) {
-      var block = $(`#hour-${i}`);
-      if (!block.length) continue;
+  // function updateHourlyBlocks() {
+  //   var currentHour = dayjs().hour();
+  //   for (var i = 9; i <= 17; i++) {
+  //     var block = $(`#hour-${i}`);
+  //     if (!block.length) continue;
 
-      block.removeClass("past present future");
-      if (i < currentHour) {
-        block.addClass("past");
-      } else if (i === currentHour) {
-        block.addClass("present");
-      } else {
-        block.addClass("future");
-      }
+  //     block.removeClass("past present future");
+  //     if (i < currentHour) {
+  //       block.addClass("past");
+  //     } else if (i === currentHour) {
+  //       block.addClass("present");
+  //     } else {
+  //       block.addClass("future");
+  //     }
+  //   }
+  // }
+
+  $(".time-block").each(function() {
+    var block = $(this);
+    var id = block.attr("id").split("-")[1];
+    var currentHour = dayjs().hour();
+    if (id<currentHour ) {
+    block.addClass ("past");
+    block.removeClass ("present future")
+    } else if (id==currentHour) {
+      block.addClass ("present");
+      block.removeClass ("past future");
+    } else {
+      block.addClass ("future");
+      block.removeClass ("present past");
     }
   }
+  )
 
   function updateCurrentDay() {
     var currentDate = new Date();
@@ -27,17 +44,17 @@ $(document).ready(function () {
     $("#currentDay").text(currentDay);
   }
   updateCurrentDay();
-  updateHourlyBlocks();
+  //updateHourlyBlocks();
 });
 
 function saveText(hour) {
   var taskArea = $(`#hour-${hour} .description`);
-  localStorage.setItem(`hour-${hour}-text`, taskArea.val());
+  localStorage.setItem(`hour-${hour}`, taskArea.val());
 }
 
 function loadText(hour) {
   var taskArea = $(`#hour-${hour} .description`);
-  var savedText = localStorage.getItem(`hour-${hour}-text`);
+  var savedText = localStorage.getItem(`hour-${hour}`);
   if (savedText !== null) {
     taskArea.val(savedText);
   }
@@ -49,3 +66,6 @@ $(".saveBtn").on("click", function () {
 });
 
 
+for (let i = 9; i < 18; i++) {
+  loadText (i);
+}
